7d8acb1836b265153292f55e56c08f4915d41949
andreyishchenko <andreyishchenko@sprut.ru>
2023-05-12 19:16:55 +0300

    install jwt-decode

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { JwtService } from '@nestjs/jwt';
 import { User } from 'src/schemas/users.schema';
 import { UsersService } from '../users/users.service';
 import { jwtConstants } from './constants';
+import jwt_decode from 'jwt-decode';
 
 @Injectable()
 export class AuthService {
@@ -47,27 +48,30 @@ export class AuthService {
     }
   }
 
-  parseJwt(token: string) {
-    try {
-      const base64Url = token.split('.')[1];
-      const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-      const jsonPayload = decodeURIComponent(
-        atob(base64)
-          .split('')
-          .map(function (c) {
-            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-          })
-          .join(''),
-      );
+  // parseJwt(token: string) {
+  //   try {
+  //     const base64Url = token.split('.')[1];
+  //     const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  //     const jsonPayload = decodeURIComponent(
+  //       atob(base64)
+  //         .split('')
+  //         .map(function (c) {
+  //           return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  //         })
+  //         .join(''),
+  //     );
 
-      return JSON.parse(jsonPayload);
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  //     return JSON.parse(jsonPayload);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
 
   async getUserByTokenData(token: string): Promise<User> {
-    const parsedTokenData = this.parseJwt(token);
+    // const parsedTokenData = this.parseJwt(token);
+    const parsedTokenData = jwt_decode(token) as any;
+
+    console.log(parsedTokenData);
 
     return await this.usersService.findOne(parsedTokenData.user.username);
   }
