bf2cdf06e39d4fb5c9dde6e158944270ee7b688d
andreyishchenko <andreyishchenko@sprut.ru>
2023-05-12 19:01:30 +0300

    update atob

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -48,18 +48,22 @@ export class AuthService {
   }
 
   parseJwt(token: string) {
-    const base64Url = token.split('.')[1];
-    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    const jsonPayload = decodeURIComponent(
-      atob(base64)
-        .split('')
-        .map(function (c) {
-          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-        })
-        .join(''),
-    );
+    try {
+      const base64Url = token.split('.')[1];
+      const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+      const jsonPayload = decodeURIComponent(
+        atob(base64)
+          .split('')
+          .map(function (c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+          })
+          .join(''),
+      );
 
-    return JSON.parse(jsonPayload);
+      return JSON.parse(jsonPayload);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   async getUserByTokenData(token: string): Promise<User> {
